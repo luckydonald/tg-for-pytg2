8a533b9ea2d0284daa429e1e40094e35da0a9c43
luckydonald <luckydonald@flutterb.at>
2015-01-27 18:28:39 +0100

    Done....?

--- msg-server-tg.c
+++ msg-server-tg.c
@@ -24,97 +24,54 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+
+//close():
+#include <unistd.h>
+
 //exit():
 #include <stdlib.h>
 #include "tgl/tgl-layout.h"
+#include "tgl/tgl.h"
+
+// while debug:
+//#include "tgl/tgl.h"
+
+// va_list, va_start, va_arg, va_end
+#include <stdarg.h>
+#include <arpa/inet.h>
+#include <assert.h>
+
+// string length strnlen()
+#include <string.h>
 
 
 #define SOCKET_ANSWER_MAX_SIZE (1 << 25)
 static char socket_answer[SOCKET_ANSWER_MAX_SIZE + 1];
 static int socket_answer_pos = -1;
 
+extern struct tgl_state *TLS;
+
+
 
 //packet size
 int socked_fd;
 struct sockaddr_in serv_addr;
 
-#define DEF_PORT 4242
-#define DEF_ADRR "127.0.0.1"
+#define DEFAULT_PORT 4458
 
-void socket_init (const char *address_string);
+void socket_init (char *address_string);
+void socket_send(char *message);
 
 void lua_init(const char *address_string);
 void lua_new_msg (struct tgl_message *M);
 
-int main(int argc, char *argv[])
-{
-	socket_init("127.0.0.1:4458");
-	if( send(socked_fd , &socket_answer , SOCKET_ANSWER_MAX_SIZE , 0) < 0)
-	{
-		puts("Send failed");
-		return 1;
-	}
-	close(socked_fd);
-	return 0;
-}
-
 void lua_init (const char *address_string) {
-	socket_init(address_string);
+	char *address_string_copy = malloc(sizeof(char) * strnlen(address_string,23));
+	strcpy(address_string_copy, address_string);
+	socket_init(address_string_copy);
 }
-void lua_new_msg (struct tgl_message *M) {
-	if (!have_file) { return; }
-	push("{", NULL);
-	push_message (M);
-	push("}", NULL);
-}
-
-void socket_init (const char *address_string) {
-	if (address_string == NULL) {
-		printf("No address and no port given.\n");
-		exit(3);
-	}
-	char *port_pos = NULL;
-	uint16_t port = DEFAULT_PORT;
-	strtok(address_string, ":");
-	port_pos = strtok(NULL, ":"); //why is it needed doubled?
-	if (port_pos == NULL) {
-		printf("Address: '%s', no port given, using port %i instead.\n" , address_string, port);
-	} else {
-		port = atoi (port_pos);
-		printf("Address: '%s', IP: %i.\n", address_string,port);
-	}
-
-	socked_fd = socket(AF_INET, SOCK_STREAM, 0); //lets do UDP Socket and listener_d is the Descriptor
-	if (socked_fd == -1)
-	{
-		printf("I can't open the socket!");
-		exit(2);
-	}
-	memset(serv_addr, 0, sizeof(struct sockaddr_in));
-	serv_addr.sin_family = AF_INET; //still TCP
-	serv_addr.sin_port = (in_port_t) htons((uint16_t) DEF_PORT); //The evil port.
-	int connection = connect(socked_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	inet_pton(AF_INET, address_string, &(serv_addr.sin_addr)); //copy the adress.
-	serv_addr.sin_port = (in_port_t) htons((uint16_t)port);
-	if (connection == -1) {
-		printf("I can't connect!");
-		exit(6);
-	}
-
-}
-void socket_send(char *message){
-	if( send(socked_fd , &socket_answer , SOCKET_ANSWER_MAX_SIZE , 0) < 0)
-	{
-		puts("Send failed");
-	}
-}
-
-
-
-
-void socket_answer_start (void) {
-	socket_answer_pos = 0;
-}
+void push_message (struct tgl_message *M);
+void expand_escapes_alloc(char* dest, const char* src);
 
 static void socket_answer_add_printf (const char *format, ...) __attribute__ ((format (printf, 1, 2)));
 void socket_answer_add_printf (const char *format, ...) {
@@ -126,29 +83,128 @@ void socket_answer_add_printf (const char *format, ...) {
 	if (socket_answer_pos > SOCKET_ANSWER_MAX_SIZE) { socket_answer_pos = -1; }
 }
 
-void socket_answer_end (struct in_ev *ev) {
-	if (ev->bev && socket_answer_pos > 0) {
-		static char s[100];
-		sprintf (s, "ANSWER %d\n", socket_answer_pos);
-		bufferevent_write (ev->bev, s, strlen (s));
-		bufferevent_write (ev->bev, socket_answer, socket_answer_pos);
-		bufferevent_write (ev->bev, "\n", 1);
-	}
-	socket_answer_pos = -1;
-}
 
 #define push(...) \
   socket_answer_add_printf (__VA_ARGS__)
 
+void lua_new_msg (struct tgl_message *M) {
+	//TODO has socket? else just return.
+	//socket_answer_start ();
+	printf("Sending Message...\n");
+	socket_answer_pos = 0;
+	push("{", NULL);
+	push_message (M);
+	push("}", NULL);
+	if (socket_answer_pos > 0) {
+		printf("Message length: %i\n", socket_answer_pos);
+		if( send(socked_fd, &socket_answer, socket_answer_pos , 0) < 0)
+		{
+			puts("Send failed.");
+		} else {
+			puts("Did send.");
+		}
+	}
+	socket_answer_pos = -1;
+}
+
+void socket_init (char *address_string) {
+	if (address_string == NULL) {
+		printf("No address and no port given.\n");
+		exit(3);
+	}
+	char *port_pos = NULL;
+	uint16_t port = DEFAULT_PORT;
+	strtok(address_string, ":");
+	port_pos = strtok(NULL, ":"); //why is it needed doubled?
+	if (port_pos == NULL) {
+		printf("Address: \"%s\", no port given, using port %i instead.\n" , address_string, port);
+	} else {
+		port = atoi (port_pos);
+		printf("Address: \"%s\", IP: %i.\n", address_string,port);
+	}
+
+	socked_fd = socket(AF_INET, SOCK_STREAM, 0); //lets do UDP Socket and listener_d is the Descriptor
+	if (socked_fd == -1)
+	{
+		printf("I can't open the socket!\n");
+		exit(5);
+	} else {
+		printf("Socket opened.\n");
+	}
+	memset((void *) &serv_addr, 0, sizeof(struct sockaddr_in));
+	serv_addr.sin_family = AF_INET; //still TCP
+	inet_pton(AF_INET, address_string, &(serv_addr.sin_addr)); //copy the adress.
+	serv_addr.sin_port = (in_port_t) htons((uint16_t)port);
+	int connection = connect(socked_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	if (connection == -1) {
+		printf("I can't connect!\n");
+		exit(6);
+	}
+
+}
 
 
 
 
 
+
+
+char *format_peer_type (int x) {
+	switch (x) {
+		case TGL_PEER_USER:
+			return "user";
+		case TGL_PEER_CHAT:
+			return "chat";
+		case TGL_PEER_ENCR_CHAT:
+			return "encr_chat";
+		default:
+			assert (0);
+			return("Nope.avi");
+	}
+}
+char *format_bool(int bool) {
+	return (bool ? "true": "false");
+}
+char *format_string_or_null(char *str) {
+	if (str == NULL) {
+		return "";
+	} else {
+		return str;
+	}
+}
+
+void push_peer (tgl_peer_id_t id, tgl_peer_t *P);
+
+void push_user (tgl_peer_t *P) {
+	push("\"first_name\":\"%s\", \"last_name\":\"%s\", \"real_first_name\": \"%s\", \"real_last_name\": \"%s\", \"phone\":\"%s\"%s",
+			P->user.first_name, P->user.last_name, format_string_or_null(P->user.real_first_name), format_string_or_null(P->user.real_last_name),  format_string_or_null(P->user.phone), (P->user.access_hash ? ", \"access_hash\":1":""));
+}
+void push_chat (tgl_peer_t *P) {
+	assert (P->chat.title);
+	push("\"title\":\"%s\", \"members_num\":%i",
+			P->chat.title, P->chat.users_num);
+	if (P->chat.user_list) {
+		push(", \"members\": [", NULL);
+		int i;
+		for (i = 0; i < P->chat.users_num; i++) {
+			if (i != 0) {
+				push(", ", NULL);
+			}
+			tgl_peer_id_t id = TGL_MK_USER (P->chat.user_list[i].user_id);
+			push_peer (id, tgl_peer_get (TLS, id));
+
+		}
+		push("]", NULL);
+	}
+}
+void push_encr_chat (tgl_peer_t *P) {
+	push ("\"user\": ");
+	push_peer (TGL_MK_USER (P->encr_chat.user_id), tgl_peer_get (TLS, TGL_MK_USER (P->encr_chat.user_id)));
+}
 
 void push_peer (tgl_peer_id_t id, tgl_peer_t *P) {
 	push("{");
-	push("'id':%i, 'type':'%s', 'print_name': '", tgl_get_peer_id (id), format_peer_type (tgl_get_peer_type (id)));
+	push("\"id\":%i, \"type\":\"%s\", \"print_name\": \"", tgl_get_peer_id (id), format_peer_type (tgl_get_peer_type (id)));
 	if (!P || !(P->flags & FLAG_CREATED)) {
 		switch (tgl_get_peer_type (id)) {
 			case TGL_PEER_USER:
@@ -163,14 +219,14 @@ void push_peer (tgl_peer_id_t id, tgl_peer_t *P) {
 			default:
 				assert (0);
 		}
-		push("'}", NULL);
+		push("\"}", NULL);
 		return;
 
 	}
 
 	//P is defined did not return.
 
-	push("%s', 'flags':%i", P->print_name, P->flags);
+	push("%s\", ", P->print_name);
 
 	switch (tgl_get_peer_type(id))
 	{
@@ -187,88 +243,16 @@ void push_peer (tgl_peer_id_t id, tgl_peer_t *P) {
 			assert(0);
 	}
 
-	push("', 'flags': %i}",  P->flags);
-}
-void push_user (tgl_peer_t *P) {
-	push("'first_name':'%s', 'last_name':'%s', 'real_first_name': '%s', 'real_last_name': '%s', 'phone','%s'%s",
-			P->user.first_name, P->user.last_name, P->user.real_first_name, P->user.real_last_name,  P->user.phone, (P->user.access_hash ? " 'access_hash':1":""));
+	push(", \"flags\": %i}",  P->flags);
 }
 
-void push_chat (tgl_peer_t *P) {
-	assert (P->chat.title);
-	push("'title':'%s', 'members_num':%i",
-			P->chat.title, P->chat.users_num);
-	if (P->chat.user_list) {
-		push(", 'members': [", NULL);
-		int i;
-		for (i = 0; i < P->chat.users_num; i++) {
-			if (i != 0) {
-				push(", ", NULL);
-			}
-			tgl_peer_id_t id = TGL_MK_USER (P->chat.user_list[i].user_id);
-			push_peer (id, tgl_peer_get (TLS, id));
-
-		}
-		push("]", NULL);
-	}
-}
-
-void push_encr_chat (tgl_peer_t *P) {
-	 push ("'user': ");
-	push_peer (TGL_MK_USER (P->encr_chat.user_id), tgl_peer_get (TLS, TGL_MK_USER (P->encr_chat.user_id)));
-}
-
-char *format_peer_type (int x) {
-	switch (x) {
-		case TGL_PEER_USER:
-			return "user";
-		case TGL_PEER_CHAT:
-			return "chat";
-		case TGL_PEER_ENCR_CHAT:
-			return "encr_chat";
-		default:
-			assert (0);
-	}
-}
-char *format_bool(void bool) {
-	return (bool ? "true": "false");
-}
-
-
-void push_message (struct tgl_message *M) {
-	if (!(M->flags & FLAG_CREATED)) { return; }
-	push("'id':%lld, 'flags': %i, 'forward':", M->id, M->flags);
-	if (tgl_get_peer_type (M->fwd_from_id)) {
-		push("{'from': ", NULL);
-		push_peer (M->fwd_from_id, tgl_peer_get (TLS, M->fwd_from_id));
-		push(", 'date': %i}", M->fwd_date);
-	} else {
-		push("null", NULL);
-	}
-	push(", 'from':", NULL);
-	push_peer (M->from_id, tgl_peer_get (TLS, M->from_id));
-	push(", 'to':", NULL);
-	push_peer (M->to_id, tgl_peer_get (TLS, M->to_id));
-	push(", 'out': %s, 'unread':%s, 'date':%i, 'service':%s", format_bool(M->out), format_bool(M->unread), M->date, format_bool(M->service), );
-
-	if (!M->service) {
-		if (M->message_len && M->message) {
-			push(", 'text': '%.*s'", M->message_len, M->message); // http://stackoverflow.com/a/3767300 //TODO: escape!
-		}
-		if (M->media.type && M->media.type != tgl_message_media_none) {
-			push(", 'media':", NULL);
-			push_media (&M->media);
-		}
-	}
-	push("}",NULL);
-}
 
 void push_media (struct tgl_message_media *M) {
 	push("{", NULL);
 	switch (M->type) {
 		case tgl_message_media_photo:
 		case tgl_message_media_photo_encr:
-			push("'type': 'photo'", NULL);
+			push("\"type\": \"photo\"", NULL);
 			break;
 			/*case tgl_message_media_video:
 			  case tgl_message_media_video_encr:
@@ -282,23 +266,114 @@ void push_media (struct tgl_message_media *M) {
 				break;*/
 		case tgl_message_media_document:
 		case tgl_message_media_document_encr:
-			push("'type': 'document'", NULL);
+			push("\"type\": \"document\"", NULL);
 			break;
 		case tgl_message_media_unsupported:
-			push("'type': 'unsupported'", NULL);
+			push("\"type\": \"unsupported\"", NULL);
 			break;
 		case tgl_message_media_geo:
-			push("'type': 'geo', 'longitude': %i, 'latitude': %i", M->geo.longitude, M->geo.latitude);
+			push("\"type\": \"geo\", \"longitude\": %f, \"latitude\": %f", M->geo.longitude, M->geo.latitude);
 			break;
 		case tgl_message_media_contact:
-			push("'type': 'contact', 'phone': '%s', 'first_name': '%s', 'last_name': '%s', 'user_id': %i",  M->phone, M->first_name, M->last_name, M->user_id);
+			push("\"type\": \"contact\", \"phone\": \"%s\", \"first_name\": \"%s\", \"last_name\": \"%s\", \"user_id\": %i",  M->phone, M->first_name, M->last_name, M->user_id);
 			break;
 		default:
-			push("'type': '???'", NULL);
+			push("\"type\": \"\?\?\?\"", NULL); //escaped "???" to avoid Trigraph. (see http://stackoverflow.com/a/1234618 )
 			break;
 	}
 	push("}", NULL);
 }
+
+void push_message (struct tgl_message *M) {
+	if (!(M->flags & FLAG_CREATED)) { return; }
+	push("\"id\":%lld, \"flags\": %i, \"forward\":", M->id, M->flags);
+	if (tgl_get_peer_type (M->fwd_from_id)) {
+		push("{\"from\": ", NULL);
+		push_peer (M->fwd_from_id, tgl_peer_get (TLS, M->fwd_from_id));
+		push(", \"date\": %i}", M->fwd_date);
+	} else {
+		push("null", NULL);
+	}
+	push(", \"from\":", NULL);
+	push_peer (M->from_id, tgl_peer_get (TLS, M->from_id));
+	push(", \"to\":", NULL);
+	push_peer (M->to_id, tgl_peer_get (TLS, M->to_id));
+	push(", \"out\": %s, \"unread\":%s, \"date\":%i, \"service\":%s", format_bool(M->out), format_bool(M->unread), M->date, format_bool(M->service) );
+
+	if (!M->service) {
+		if (M->message_len && M->message) {
+			push(", \"text\": \"%.*s\"", M->message_len, expand_escapes_alloc(M->message)); // http://stackoverflow.com/a/3767300 //TODO: escape!
+		}
+		if (M->media.type && M->media.type != tgl_message_media_none) {
+			push(", \"media\":", NULL);
+			push_media (&M->media);
+		}
+	}
+	push("}",NULL);
+}
+
+// http://stackoverflow.com/a/3535143
+void expand_escapes(char* dest, const char* src)
+{
+	char c;
+
+	while (c = *(src++)) {
+		switch(c) {
+			case '\a':
+				*(dest++) = '\\';
+				*(dest++) = 'a';
+				break;
+			case '\b':
+				*(dest++) = '\\';
+				*(dest++) = 'b';
+				break;
+			case '\t':
+				*(dest++) = '\\';
+				*(dest++) = 't';
+				break;
+			case '\n':
+				*(dest++) = '\\';
+				*(dest++) = 'n';
+				break;
+			case '\v':
+				*(dest++) = '\\';
+				*(dest++) = 'v';
+				break;
+			case '\f':
+				*(dest++) = '\\';
+				*(dest++) = 'f';
+				break;
+			case '\r':
+				*(dest++) = '\\';
+				*(dest++) = 'r';
+				break;
+			case '\\':
+				*(dest++) = '\\';
+				*(dest++) = '\\';
+				break;
+			case '\"':
+				*(dest++) = '\\';
+				*(dest++) = '\"';
+				break;
+			case '\'':
+				*(dest++) = '\\';
+				*(dest++) = '\'';
+				break;
+			default:
+				*(dest++) = c;
+		}
+	}
+
+	*dest = '\0'; /* Ensure nul terminator */
+}
+/* Returned buffer may be up to twice as large as necessary */
+char* expand_escapes_alloc(const char* src)
+{
+	char* dest = malloc(2 * strlen(src) + 1);
+	expand_escapes(dest, src);
+	return dest;
+}
+
 /*
 void push_message (struct tgl_message *M) {
 	assert (M);
@@ -550,3 +625,26 @@ void check () {
 }
 
 */
+
+// Empty stuff:
+void lua_secret_chat_update (struct tgl_secret_chat *C, unsigned flags) {
+	return;
+}
+void lua_user_update (struct tgl_user *U, unsigned flags) {
+	return;
+}
+void lua_diff_end (void) {
+	return;
+}
+void lua_do_all (void) {
+	return;
+}
+void lua_our_id (int id) {
+	return;
+}
+void lua_binlog_end (void) {
+	return;
+}
+void lua_chat_update (struct tgl_chat *C, unsigned flags) {
+	return;
+}
---
+++ msg-server-tg.h
@@ -0,0 +1,16 @@
+#ifndef __LUA_TG_H__
+#define __LUA_TG_H__
+
+#include <string.h>
+#include <tgl/tgl.h>
+
+void lua_init (const char *file);
+void lua_new_msg (struct tgl_message *M);
+void lua_our_id (int id);
+void lua_secret_chat_update (struct tgl_secret_chat *U, unsigned flags);
+void lua_user_update (struct tgl_user *U, unsigned flags);
+void lua_chat_update (struct tgl_chat *C, unsigned flags);
+void lua_binlog_end (void);
+void lua_diff_end (void);
+void lua_do_all (void);
+#endif
